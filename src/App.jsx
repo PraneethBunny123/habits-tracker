064a213e725fc9a757bc4f64ca38a83c547a0324
praneeth <gp.gunturu@gmail.com>
2025-08-02 17:36:32 -0500

    added reducer to store starting with App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Provider } from "react-redux"
 import store from './store/store'
+import { Container, Typography } from "@mui/material"
 
 function App() {
     return (
         <Provider store={store}>
-            <div>Hello</div>
+            <Container maxWidth="md">
+                <Typography component="h1" variant="h2" align="center">
+                    Habits Tracker
+                </Typography>
+            </Container>
         </Provider>
     )
 }
