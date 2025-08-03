89e68f7f44e20b539db8870cfa88d0b341eb6e1c
praneeth <gp.gunturu@gmail.com>
2025-08-02 21:17:48 -0500

    few styles and outline

--- src/components/HabitStats.jsx
+++ src/components/HabitStats.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchHabits } from "../store/habit-slice"
+import { Paper, Typography } from "@mui/material"
 
 export default function HabitStats() {
     const {habits, isLoading, error} = useSelector((state) => state.habits)
@@ -12,6 +13,19 @@ export default function HabitStats() {
     }, [])
 
     return (
-        <div>Stats</div>
+        <Paper elevation={2} sx={{p: 2, mt: 4}}>
+            <Typography variant="h6" gutterBottom>
+                Habit Stats
+            </Typography>
+            <Typography variant="body1">
+                Total Habits:
+            </Typography>
+            <Typography variant="body1">
+                Completed Today: 
+            </Typography>
+            <Typography variant="body1">
+                Total Habits:
+            </Typography>
+        </Paper>
     )
 }
