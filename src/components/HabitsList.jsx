88a3e17e5431802c80bd5c6c9a7cc8e33699a564
praneeth <gp.gunturu@gmail.com>
2025-08-02 20:43:39 -0500

    a function to check for streak

--- src/components/HabitsList.jsx
+++ src/components/HabitsList.jsx
@@ -10,6 +10,22 @@ export default function HabitsList() {
 
     const today = new Date().toISOString().split("T")[0]
 
+    function getStreak(habit) {
+        let streak = 0;
+        const currentDate = new Date()
+
+        while(true) {
+            const dateString = currentDate.toISOString().split('T')[0]
+
+            if(habit.completedDates.includes(dateString)) {
+                streak++;
+                currentDate.setDate(currentDate.getDate() - 1)
+            } else {
+                break;
+            }
+        }
+    }
+
     return (
         <Box sx={{
             display: 'flex',
