cc26357001fec03e6d598b2e9b9db0728f71ab35
praneeth <gp.gunturu@gmail.com>
2025-08-02 21:23:24 -0500

    displaying the stats: displaying the total stats, completed Stats

--- src/components/HabitStats.jsx
+++ src/components/HabitStats.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchHabits } from "../store/habit-slice"
-import { Paper, Typography } from "@mui/material"
+import { LinearProgress, Paper, Typography } from "@mui/material"
 
 export default function HabitStats() {
     const {habits, isLoading, error} = useSelector((state) => state.habits)
@@ -12,19 +12,32 @@ export default function HabitStats() {
         dispatch(fetchHabits())
     }, [])
 
+    function getCompletedToday() {
+        const today = new Date().toISOString().split('T')[0]
+        return habits.filter(habit => habit.completedDates.includes(today)).length
+    }
+
+    if(isLoading) {
+        return <LinearProgress />
+    }
+
+     if(error) {
+        return <Typography color="error">{error}</Typography>
+     }
+
     return (
         <Paper elevation={2} sx={{p: 2, mt: 4}}>
             <Typography variant="h6" gutterBottom>
                 Habit Stats
             </Typography>
             <Typography variant="body1">
-                Total Habits:
+                Total Habits: {habits.length}
             </Typography>
             <Typography variant="body1">
-                Completed Today: 
+                Completed Today: {getCompletedToday()}
             </Typography>
             <Typography variant="body1">
-                Total Habits:
+                Longest Streak:
             </Typography>
         </Paper>
     )
